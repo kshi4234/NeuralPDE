a3fbb2abff58265d32169de908329c58c9442865
kshi4234 <57573803+kshi4234@users.noreply.github.com>
2024-11-14 15:41:07 -0500

    Moving to GP Branch

--- utils/loss_utils.py
+++
@@ -1,6 +0,0 @@
-# Define loss functions in this file depending on type of PDE.
-
-# Generic loss function class. Can be implemented by specific loss functions
-class Generic_Loss():
-    def __init__():
-        return
